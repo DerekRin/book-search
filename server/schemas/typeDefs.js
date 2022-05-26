df46de88d09f56d50f91c3eda5caae6183a6624d
DerekRin <derekrin98@gmail.com>
2022-05-25 21:55:36 -0500

    All the time I have for

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,44 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    _id: ID
+    description: String
+    title: String
+    image: String
+    authors: [String]
+    link: link
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addBook(thoughtText: String!): Book
+  }
+`;
+
+module.exports = typeDefs;
